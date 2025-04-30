72d066ce14f7bc6501dc758d3dfb83f0c0ad5657
EngMomenNasr <moneznasr@gmail.com>
2025-04-30 05:08:13 +0300

    fix request error

--- client/src/components/ContantUs.jsx
+++ client/src/components/ContantUs.jsx
@@ -87,25 +87,44 @@ export default function ContactUs() {
   };
 
   return (
-    <section className="bg-slate-900 py-16 px-6 md:px-10">
-      {modal.isOpen && (
-        <div className="fixed inset-0 bg-[rgba(0,0,0,0.5)] flex items-center justify-center z-50">
-          <div className={`flex flex-col justify-center items-center bg-slate-900 p-6 rounded-lg max-w-sm w-full border-2 ${modal.isError ? "border-red-500" : "border-slate-500"}`}>
-            <h2 className="text-white text-lg font-bold text-center">
-              {modal.isError ? "Error" : "Message Sent Successfully!"}
-            </h2>
-            <img src={modal.isError ? "/worng.svg" : "/success.svg"} width="150px" className='bg-red-300 rounded-full mt-5' />
-            <pre className="text-gray-300 text-center mt-2 whitespace-pre-wrap">{modal.message}</pre>
-            <div className="text-center mt-4">
-              {modal.isError ? (
-                <button onClick={closeModal} className="bg-red-600 hover:bg-red-700 py-2 px-6 rounded text-white cursor-pointer">Close</button>
-              ) : (
-                <Link to="/" onClick={closeModal} className="bg-red-600 hover:bg-red-700 py-2 px-6 rounded text-white cursor-pointer">Close</Link>
-              )}
-            </div>
-          </div>
-        </div>
-      )}
-    </section>
+     <section className="bg-slate-900 py-16 px-6 md:px-10">
+
+     {modal.isOpen && (
+    
+     <div className="fixed inset-0 bg-[rgba(0,0,0,0.5)] flex items-center justify-center z-50">
+    
+     <div className={`flex flex-col justify-center items-center bg-slate-900 p-6 rounded-lg max-w-sm w-full border-2 ${modal.isError ? "border-red-500" : "border-slate-500"}`}>
+    
+     <h2 className="text-white text-lg font-bold text-center">
+    
+     {modal.isError ? "Error" : "Message Sent Successfully!"}
+    
+     </h2>
+    
+     <img src={modal.isError ? "/worng.svg" : "/success.svg"} width="150px" className='bg-red-300 rounded-full mt-5' />
+    
+     <pre className="text-gray-300 text-center mt-2 whitespace-pre-wrap">{modal.message}</pre>
+    
+     <div className="text-center mt-4">
+    
+     {modal.isError ? (
+    
+     <button onClick={closeModal} className="bg-red-600 hover:bg-red-700 py-2 px-6 rounded text-white cursor-pointer">Close</button>
+    
+     ) : (
+    
+    <Link to="/" onClick={closeModal} className="bg-red-600 hover:bg-red-700 py-2 px-6 rounded text-white cursor-pointer">Close</Link>
+    
+     )}
+    
+    </div>
+    
+     </div>
+    
+     </div>
+    
+     )}
+    
+     </section>
   );
 }
